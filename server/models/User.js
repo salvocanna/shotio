cf32e230455a64af372a81658a12b4312819474f
Salvo <salvo.cannamela@gmail.com>
2017-08-12 18:21:12 +0100

    Trying out apollo client and client-pub-sub over WS pt2

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('User', userSchema);
+
---
+++ server/resolvers.js
@@ -0,0 +1,66 @@
+import connectors from './connectors';
+// import User from './models/User';
+
+export default {
+    Query: {
+        users() {
+            return connectors.User.getUsers()
+                .then((users) => {
+                    return users.map((user) => {
+                        return {
+                            id: user._id,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            email: user.email
+                        };
+                    });
+                })
+                .catch((err) => {
+                    throw new Error(err);
+                });
+        }
+    },
+    Mutation: {
+        signUp(root, args) {
+            const errors = [];
+
+            return connectors.Auth.signUp(args)
+                .then(token => ({
+                    token,
+                    errors
+                }))
+                .catch((err) => {
+                    if (err.code && err.message) {
+                        errors.push({
+                            key: err.code,
+                            value: err.message
+                        });
+                        return { token: null, errors };
+                    }
+
+                    throw new Error(err);
+                });
+        },
+        signIn(root, args) {
+            const errors = [];
+
+            return connectors.Auth.signIn(args)
+                .then(token => ({
+                    token,
+                    errors
+                }))
+                .catch((err) => {
+                    if (err.code && err.message) {
+                        errors.push({
+                            key: err.code,
+                            value: err.message
+                        });
+
+                        return { token: null, errors };
+                    }
+
+                    throw new Error(err);
+                });
+        }
+    }
+};
